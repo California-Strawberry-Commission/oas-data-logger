32196b0225d97e3320e1930a42fa1fe58da9b188
Alex Herman <1wheelalex@gmail.com>
2025-07-22 10:33:38 -0700

    attempting to change the map

--- software/data-visualizer/components/map.tsx
+++ software/data-visualizer/components/map.tsx
@@ -50,15 +50,17 @@ export default function Map({
   return (
     <MapContainer
       center={center}
-      zoom={13}
+      zoom={18} // Zoom level increased from 13 to 16
       scrollWheelZoom={true}
       touchZoom={true}
       doubleClickZoom={true}
       className="h-full w-full"
     >
+      {/* Switched to a satellite tile layer */}
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+        url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+        maxZoom={22}
       />
       <Polyline positions={points} color="blue" />
       <Marker position={points[0]} icon={greenIcon}>
