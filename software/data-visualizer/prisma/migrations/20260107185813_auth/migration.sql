050390bd96429ea8f6b869b172683271ec1bcb50
bholt13 <holt0316@gmail.com>
2026-01-07 10:59:59 -0800

    [data-visualizer][prisma] Modify DB schema to support authentication of devices for upload.

---
+++ software/data-visualizer/prisma/migrations/20260107185813_auth/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "DeviceSecret" (
+    "deviceId" TEXT NOT NULL,
+    "secret" TEXT NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "DeviceSecret_pkey" PRIMARY KEY ("deviceId")
+);
+
+-- AddForeignKey
+ALTER TABLE "DeviceSecret" ADD CONSTRAINT "DeviceSecret_deviceId_fkey" FOREIGN KEY ("deviceId") REFERENCES "Device"("id") ON DELETE CASCADE ON UPDATE CASCADE;
