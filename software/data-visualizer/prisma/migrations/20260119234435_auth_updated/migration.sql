47d669cbbdf69101db97229cd581052132434699
bholt13 <holt0316@gmail.com>
2026-01-20 11:17:04 -0800

    Added createdAt, encryptionKeyVersion, and removed axios from package.json.

---
+++ software/data-visualizer/prisma/migrations/20260119234435_auth_updated/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "DeviceSecret" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "encryptionKeyVersion" INTEGER NOT NULL DEFAULT 1;
