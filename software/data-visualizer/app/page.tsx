1dee28daf697df45a53ebdd6aff6bdfaee671e43
Genki Kondo <kondo.genki@gmail.com>
2026-02-11 12:07:36 +0000

    [data-viz] Add logo

--- software/data-visualizer/app/page.tsx
+++ software/data-visualizer/app/page.tsx
@@ -1,8 +1,9 @@
-import AdminButton from "@/components/top-bar/admin-button";
 import LoginModal from "@/components/login-modal";
-import AccountButton from "@/components/top-bar/account-button";
 import MainContent from "@/components/main-content";
+import AccountButton from "@/components/top-bar/account-button";
+import AdminButton from "@/components/top-bar/admin-button";
 import { getCurrentUser } from "@/lib/auth";
+import Image from "next/image";
 
 export default async function Home() {
   const user = await getCurrentUser();
@@ -20,9 +21,14 @@ export default async function Home() {
     <div className="flex flex-col h-screen">
       {/* Top app bar */}
       <header className="w-full border-b bg-background">
-        <div className="max-w-7xl mx-auto flex items-center justify-end gap-2 h-14 px-4">
-          {isAdmin && <AdminButton />}
-          <AccountButton email={user.email} />
+        <div className="flex items-center justify-between h-14 px-4">
+          <div className="flex items-center">
+            <Image src="/logo.png" alt="Logo" width={44} height={44} priority />
+          </div>
+          <div className="flex items-center gap-2">
+            {isAdmin && <AdminButton />}
+            <AccountButton email={user.email} />
+          </div>
         </div>
       </header>
 
