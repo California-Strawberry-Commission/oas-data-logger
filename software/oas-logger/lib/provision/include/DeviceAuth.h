66a029e7a82b9d74fbb44b3f4b1e046c28287bdb
bholt13 <holt0316@gmail.com>
2026-05-11 14:50:12 -0700

    [oas-logger][DeviceAuth] Added provisioning helpers for duplicated logic, added functionality for pulling secret from already provisioned device, moved unnecessarily public functions to private.

--- software/oas-logger/lib/provision/include/DeviceAuth.h
+++ software/oas-logger/lib/provision/include/DeviceAuth.h
@@ -11,18 +11,25 @@ class DeviceAuth {
  public:
   DeviceAuth(const char* deviceId);
 
-  // secretBuffer must have room for 65 bytes at minimum
-  // 64 for secret + 1 for null terminator
-  bool loadSecret(char* secretBuffer, size_t secretBufferLen);
-
-  bool awaitProvisioning(char* secretBuffer, size_t secretBufferLen);
-
   bool loadSecretOrProvision(char* secretBuffer, size_t secretBufferLen,
                              bool rebootOnProvision = true);
 
  private:
   char deviceId_[65];
 
+  // secretBuffer must have room for 65 bytes at minimum
+  // 64 for secret + 1 for null terminator
+  bool loadSecret(char* secretBuffer, size_t secretBufferLen);
+
+  bool awaitProvisioning(char* secretBuffer, size_t secretBufferLen);
+
   void saveSecret(const char* secret);
+
+  bool readSerialInput(char* input, size_t inputLen);
+
+  bool isValidSecret(const char* secret, size_t secretLen);
+
+  bool exportProvisionedSecret(const char* secretBuffer,
+                               size_t secretBufferLen);
 };
 }  // namespace device_auth
