cec713765f32a104c1d7600d103bdd55bb690152
bholt13 <holt0316@gmail.com>
2026-06-02 08:34:42 -0700

    [factory_v1] FastLED removed.

--- software/oas-logger/src/factory_v1/main.cpp
+++ software/oas-logger/src/factory_v1/main.cpp
@@ -1,5 +1,4 @@
 #include <Arduino.h>
-#include <FastLED.h>
 #include <WiFiManager.h>
 
 #include "DeviceAuth.h"
@@ -12,12 +11,93 @@ const unsigned long SERIAL_BAUD_RATE{115200};
 // Input pins
 const gpio_num_t PIN_USER_BUTTON{GPIO_NUM_0};
 
-// NeoPixel LED
-#define LED_PIN PIN_NEOPIXEL
-#define LED_TYPE WS2812
-#define LED_COLOR_ORDER GRB
-const int NUM_LEDS{1};
-const uint8_t LED_BRIGHTNESS{10};
+// Rev 2 passive common - anode RGB LED
+const gpio_num_t PIN_LED_R{GPIO_NUM_40};
+const gpio_num_t PIN_LED_G{GPIO_NUM_38};
+const gpio_num_t PIN_LED_B{GPIO_NUM_39};
+
+constexpr int LEDC_CH_R{0};
+constexpr int LEDC_CH_G{1};
+constexpr int LEDC_CH_B{2};
+constexpr int LEDC_FREQ{5000};
+constexpr int LEDC_RES{8};
+constexpr uint8_t LED_MAX_DUTY{20};
+
+struct LedColor {
+  uint8_t r;
+  uint8_t g;
+  uint8_t b;
+};
+
+constexpr LedColor LED_OFF{0, 0, 0};
+constexpr LedColor LED_WHITE{255, 255, 255};
+constexpr LedColor LED_RED{255, 0, 0};
+constexpr LedColor LED_GREEN{0, 255, 0};
+constexpr LedColor LED_BLUE{0, 0, 255};
+constexpr LedColor LED_YELLOW{255, 255, 0};
+constexpr LedColor LED_ORANGE{255, 80, 0};
+
+static bool ledcAttached{false};
+
+static void ledcAttachAll() {
+  if (ledcAttached) return;
+
+  ledcSetup(LEDC_CH_R, LEDC_FREQ, LEDC_RES);
+  ledcSetup(LEDC_CH_G, LEDC_FREQ, LEDC_RES);
+  ledcSetup(LEDC_CH_B, LEDC_FREQ, LEDC_RES);
+
+  ledcAttachPin(PIN_LED_R, LEDC_CH_R);
+  ledcAttachPin(PIN_LED_G, LEDC_CH_G);
+  ledcAttachPin(PIN_LED_B, LEDC_CH_B);
+
+  ledcAttached = true;
+}
+
+static void ledcDetachAll() {
+  if (!ledcAttached) return;
+
+  ledcDetachPin(PIN_LED_R);
+  ledcDetachPin(PIN_LED_G);
+  ledcDetachPin(PIN_LED_B);
+
+  ledcAttached = false;
+}
+
+static void setLedColor(LedColor color) {
+  static LedColor lastColor = {0, 0, 0};
+
+  if (color.r == lastColor.r && color.g == lastColor.g &&
+      color.b == lastColor.b) {
+    return;
+  }
+  lastColor = color;
+
+  if (color.r == 0 && color.g == 0 && color.b == 0) {
+    ledcDetachAll();
+
+    pinMode(PIN_LED_R, OUTPUT);
+    pinMode(PIN_LED_G, OUTPUT);
+    pinMode(PIN_LED_B, OUTPUT);
+
+    digitalWrite(PIN_LED_R, HIGH);
+    digitalWrite(PIN_LED_G, HIGH);
+    digitalWrite(PIN_LED_B, HIGH);
+
+    return;
+  }
+
+  ledcAttachAll();
+
+  const uint16_t r = static_cast<uint16_t>(color.r) * LED_MAX_DUTY / 255;
+  const uint16_t g = static_cast<uint16_t>(color.g) * LED_MAX_DUTY / 255;
+  const uint16_t b = static_cast<uint16_t>(color.b) * LED_MAX_DUTY / 255;
+
+  // 8-bit common anode LED, 256 = 0 in 8 bit which is off, 255 is full
+  // brightness.
+  ledcWrite(LEDC_CH_R, 256 - r);
+  ledcWrite(LEDC_CH_G, 256 - g);
+  ledcWrite(LEDC_CH_B, 256 - b);
+}
 
 // Security and Provisioning
 char deviceUid[13]{0};     // Populated at boot
@@ -29,7 +109,6 @@ const char* OTA_MANIFEST_ENDPOINT{
 const char* OTA_FIRMWARE_ENDPOINT{
     "https://oas-data-logger.vercel.app/api/ota/firmware/%s/%s/%d"};
 
-CRGB leds[NUM_LEDS];
 WiFiManager wifiManager;
 
 static bool shouldResetWifi() {
@@ -56,7 +135,7 @@ static void connectWifiWithPortal() {
     ESP.restart();
   } else {
     Serial.println("[WiFi] WiFi connected");
-    FastLED.showColor(CRGB::Yellow);
+    setLedColor(LED_YELLOW);
   }
 }
 
@@ -90,17 +169,15 @@ static void runOtaUpdate() {
   ota::OtaUpdater otaUpdater(otaConfig);
   auto res{otaUpdater.updateIfAvailable(true)};
   if (res.ok) {
-    FastLED.showColor(CRGB::Green);
+    setLedColor(LED_GREEN);
   } else {
     Serial.printf("[OTA] Error when updating firmware: %s\n", res.message);
-    FastLED.showColor(CRGB::Orange);
+    setLedColor(LED_ORANGE);
   }
 }
 
 void setup() {
-  FastLED.addLeds<LED_TYPE, LED_PIN, LED_COLOR_ORDER>(leds, NUM_LEDS);
-  FastLED.setBrightness(LED_BRIGHTNESS);
-  FastLED.showColor(CRGB::Red);
+  setLedColor(LED_RED);
 
   Serial.begin(SERIAL_BAUD_RATE);
 
